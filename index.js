0b68926cc174df3a86951798b9420f3879f763a9
smileycreations15 <git@smileycreations15.com>
2019-06-16 00:24:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ smileyjs.blockchain = loadModule("blockchain.js")
 smileyjs.mdc = loadModule("mdc") || mdc
 document.head.innerHTML += "<link href='stylesheet' href='https://unpkg.com/material-components-web@2.3.1/dist/material-components-web.min.css'>"
 smileyjs.ui = {}
+// TODO: INSERT UI CODE HERE
