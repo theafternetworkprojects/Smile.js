a79441a5d1427d1bf3985a0c2d7379e89f31127b
smileycreations15 <git@smileycreations15.com>
2019-06-16 00:21:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 window.smileyjs = {}
 smileyjs.blockchain = loadModule("blockchain.js")
+smileyjs.mdc = loadModule("mdc") || mdc
+document.head.innerHTML += "<link href='stylesheet' href='https://unpkg.com/material-components-web@2.3.1/dist/material-components-web.min.css'>"
