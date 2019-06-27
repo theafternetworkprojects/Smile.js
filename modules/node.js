006526dc3b87f690c4744deb96fadcd9d10b040f
smileycreations15 <git@smileycreations15.com>
2019-06-27 23:23:59 +0300

    Add files via upload

---
+++ modules/node.js
@@ -0,0 +1,4 @@
+module.exports.rippleColor = "rgba(255, 255, 255, 0.5)"
+module.exports.initRipple = ()=>{loadModule("paper-ripple.js")}
+// module.exports.initRipple1 = ()=>{loadModule("paper-ripple-1.js")}
+loadModule("css-loader.js")
---
+++ modules/window.js
@@ -0,0 +1,5 @@
+window.Paper = {};
+window.Paper.rippleColor = "rgba(255, 255, 255, 0.5)"
+window.Paper.initRipple = ()=>{loadModule("paper-ripple.js")}
+// window.Paper.initRipple1 = ()=>{loadModule("paper-ripple-1.js")}
+loadModule("css-loader.js")
