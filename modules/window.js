5af382f5d3ba8ddfe6fdb675c8ab1f2d9d735725
smileycreations15 <git@smileycreations15.com>
2019-06-27 23:24:42 +0300

    Delete window.js

--- modules/window.js
+++
@@ -1,5 +0,0 @@
-window.Paper = {};
-window.Paper.rippleColor = "rgba(255, 255, 255, 0.5)"
-window.Paper.initRipple = ()=>{loadModule("paper-ripple.js")}
-// window.Paper.initRipple1 = ()=>{loadModule("paper-ripple-1.js")}
-loadModule("css-loader.js")
