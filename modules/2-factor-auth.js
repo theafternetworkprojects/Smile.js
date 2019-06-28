8dd90ab375a9070fe45f271036da4fe942709ac4
smileycreations15 <git@smileycreations15.com>
2019-06-28 12:08:39 +0300

    Update 2-factor-auth.js

--- modules/2-factor-auth.js
+++ modules/2-factor-auth.js
@@ -29,7 +29,7 @@ smilejs.genToken = function(secret, validity,length = 6){
 	var encoded = BigInt(encode(secret));
 	var time = BigInt(Math.floor(new Date().getTime() / validity));
 	var secretOut = [secret,time,encoded,encoded * time];
-	var hashedSecret = smileyjs.sha.sha512(secretOut.join(""));
+	var hashedSecret = smilejs.sha.sha512(secretOut.join(""));
 	var hashes = hashedSecret.match(/.{2,2}/g);
 	var hashOut = 0;
 	for (var i = 0;i !== hashes.length;i++){
