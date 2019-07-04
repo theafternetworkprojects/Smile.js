3a350f83882232dbfd766d9a40f67fb0c72b03d5
After <45772308+AfterNetwork@users.noreply.github.com>
2019-07-04 11:17:50 -0700

    missing semicolon

--- modules/paper.js
+++ modules/paper.js
@@ -1,4 +1,4 @@
-module.exports.rippleColor = "rgba(255, 255, 255, 0.6)"
+module.exports.rippleColor = "rgba(255, 255, 255, 0.6)";
 module.exports.initRipple = ()=>{loadModule("paper-ripple.js")}
 // module.exports.initRipple1 = ()=>{loadModule("paper-ripple-1.js")}
 if (this.document) loadModule("css-loader.js")
