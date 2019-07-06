b03860ca91470b3f5643e5df88ed3bb6444046bd
smileycreations15 <git@smileycreations15.com>
2019-07-06 19:19:19 +0300

    Update paper.js

--- modules/paper.js
+++ modules/paper.js
@@ -1,5 +1,6 @@
 module.exports.rippleColor = "rgba(255, 255, 255, 0.6)";
-if (this.document) loadModule("paper-ripple.js")
+if (this.document) loadModule("paper-ripple.js");
+module.exports.initRipple = ()=>{}; 
 // module.exports.initRipple1 = ()=>{loadModule("paper-ripple-1.js")}
-if (this.document) loadModule("css-loader.js")
-if (this.document) loadModule("radio.js")
+if (this.document) loadModule("css-loader.js");
+if (this.document) loadModule("radio.js");
