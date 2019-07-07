66e57e0fd85aa6a93b996f0a876fb756760bf715
smileycreations15 <git@smileycreations15.com>
2019-07-07 13:59:17 +0300

    u

--- modules/paper-ripple.js
+++ modules/paper-ripple.js
@@ -27,7 +27,7 @@ window.ontouchstart?document.addEventListener("touchstart",(event)=>{
     effect.style.left = (x - size) + "px";
     effect.style.height = size * 2 + "px";
     effect.style.width = size * 2 + "px";
-    effect.style.background = target.getAttribute("ripple-color") || globalThis.smilejs.paper.rippleColor;
+    effect.style.background = target.getAttribute("ripple-color") || (this.globalThis || this).smilejs.paper.rippleColor;
     parent.appendChild(effect);
 
     current = parent;
@@ -74,7 +74,7 @@ window.ontouchstart?document.addEventListener("touchstart",(event)=>{
     effect.style.left = (x - size) + "px";
     effect.style.height = size * 2 + "px";
     effect.style.width = size * 2 + "px";
-    effect.style.background = target.getAttribute("ripple-color") || globalThis.smilejs.paper.rippleColor;
+    effect.style.background = target.getAttribute("ripple-color") || (this.globalThis || this).smilejs.paper.rippleColor;
     parent.appendChild(effect);
 
     current = parent;
