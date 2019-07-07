b9883243f7cd6bbb40e67399d68453413d7c4ae6
smileycreations15 <git@smileycreations15.com>
2019-07-07 14:43:06 +0300

    u

--- modules/paper-ripple.js
+++ modules/paper-ripple.js
@@ -27,9 +27,9 @@ is_touch_device()?document.addEventListener("touchstart",(event)=>{
     let target = event.target;
     while (target && target.classList && !target.classList.contains("ripple")) target = target.parentNode;
     if (!target || !target.classList || !target.classList.contains("ripple")) return;
-    if (event.touches.length > 1) return;
-    const x = event.touches[0].x - target.getBoundingClientRect().left;
-    const y = event.touches[0].y - target.getBoundingClientRect().top;
+    if (event.targetTouches.length > 1) return;
+    const x = event.targetTouches[0].screenX - target.getBoundingClientRect().left;
+    const y = event.targetTouches[0].screenY - target.getBoundingClientRect().top;
     const maxW = Math.max(x, target.offsetWidth - x);
     const maxH = Math.max(y, target.offsetHeight - y);
     const size = Math.sqrt(maxW * maxW + maxH * maxH);
@@ -57,7 +57,7 @@ is_touch_device()?document.addEventListener("touchstart",(event)=>{
     };
 
     document.ontouchmove = function (move) {
-        if (event.touches[0].x - move.touches[0].x > 4 || event.touches[0].x - move.touches[0].x < -4 || event.touches[0].y - move.touches[0].y > 4 || event.touches[0].y - move.touches[0].y < -4) {
+        if (event.targetTouches[0].screenX - move.targetTouches[0].screenX > 4 || event.targetTouches[0].screenX - move.targetTouches[0].screenX < -4 || event.targetTouches[0].screenX - move.targetTouches[0].screenY > 4 || event.targetTouches[0].screenY - move.targetTouches[0].screenY < -4) {
             clearTimeout(timeout);
             document.ontouchcancel()
         }
@@ -75,8 +75,8 @@ is_touch_device()?document.addEventListener("touchstart",(event)=>{
     while (target && target.classList && !target.classList.contains("ripple")) target = target.parentNode;
     if (!target || !target.classList || !target.classList.contains("ripple")) return;
 
-    const x = event.x - target.getBoundingClientRect().left;
-    const y = event.y - target.getBoundingClientRect().top;
+    const x = event.screenX - target.getBoundingClientRect().left;
+    const y = event.screenY - target.getBoundingClientRect().top;
     const maxW = Math.max(x, target.offsetWidth - x);
     const maxH = Math.max(y, target.offsetHeight - y);
     const size = Math.sqrt(maxW * maxW + maxH * maxH);
@@ -104,7 +104,7 @@ is_touch_device()?document.addEventListener("touchstart",(event)=>{
     };
 
     document.onpointermove = function (move) {
-        if (event.x - move.x > 4 || event.x - move.x < -4 || event.y - move.y > 4 || event.y - move.y < -4) {
+        if (event.screenX - move.x > 4 || event.screenX - move.x < -4 || event.screenY - move.y > 4 || event.screenY - move.y < -4) {
             clearTimeout(timeout);
             document.onpointercancel();
         }
