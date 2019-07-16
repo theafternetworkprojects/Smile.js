270430a5717957e41e2915001aaaf4abd9c8b08e
smileycreations15 <git@smileycreations15.com>
2019-07-16 14:09:52 +0300

    u

--- bundle-it.js
+++ bundle-it.js
@@ -12,19 +12,29 @@ const {
 function addslashes(value) {
   return (value + "").replace(/[\\"']/g, "\\$&").replace(/\u0000/g, "\\0").replace(/\n/g, "\\n").replace(/\r/g, "\\r");
 }
-bundles[0] = "(function(){", bundles[1] = "var moduleCache = {};", fs.readdir("./modules/", (canCreateDiscussions, badgeArray) => {
+bundles[0] = "(function(){", bundles[1] = "var moduleCache = {};var assets = {}", fs.readdir("./modules/", (canCreateDiscussions, badgeArray) => {
   (badgeArray || []).forEach((v) => {
     /** @type {string} */
+    console.log("Adding source file:", v)
     bundles[bundles.length] = `moduleCache["${addslashes(v)}"] = '${addslashes(fs.readFileSync("./modules/" + v) + "\n//# sourceURL=smileycreations15://smilejs/modules/" + v)}';`;
   });
-  /** @type {string} */
-  bundles[bundles.length] = "function loadModule(module1){\n    var moduleSrc = moduleCache[module1];\n  var module = {exports:{}}\n  eval(moduleSrc);\n  return module.exports\n}";
-  bundles[bundles.length] = `eval('${addslashes(fs.readFileSync("index.js").toString() + "\n//# sourceURL=smileycreations15://smilejs/index.js")}');`;
-  /** @type {string} */
-  bundles[bundles.length] = "})();";
-  /** @type {string} */
-  bundles[bundles.length] = "//# sourceURL=smileycreations15://smilejs/raw/bundle.js";
-  fs.writeFileSync("out.js", bundles.join("\n"));
-  execSync("uglifyjs -o out.min.js out.js");
-  execSync("echo >> out.min.js;echo '//# sourceURL=smileycreations15://smilejs/raw/bundle.js' >> out.min.js");
+  fs.readdir("./assets/", (canCreateDiscussions, badgeArray1) => {
+    (badgeArray1 || []).forEach((v1) => {
+      /** @type {string} */
+      console.log("Adding asset:", v1)
+      bundles[bundles.length] = `assets["${addslashes(v1)}"] = '${addslashes(fs.readFileSync("./assets/" + v1))}';`;
+      /** @type {string} */
+      bundles[bundles.length] = "function loadModule(module1){\n    var moduleSrc = moduleCache[module1];\n  var module = {exports:{}}\n  eval(moduleSrc);\n  return module.exports\n};function loadAsset(module1){\n    return assets[module1];};";
+      console.log("Adding source file:", "index.js")
+      bundles[bundles.length] = `eval('${addslashes(fs.readFileSync("index.js").toString() + "\n//# sourceURL=smileycreations15://smilejs/index.js")}');`;
+      /** @type {string} */
+      bundles[bundles.length] = "})();";
+      /** @type {string} */
+      bundles[bundles.length] = "//# sourceURL=smileycreations15://smilejs/raw/bundle.js";
+      fs.writeFileSync("out.js", bundles.join("\n"));
+      console.log("Minifying")
+      execSync("uglifyjs -o out.min.js out.js");
+      execSync("echo >> out.min.js;echo '//# sourceURL=smileycreations15://smilejs/raw/bundle.js' >> out.min.js");
+    });
+    });
 });
