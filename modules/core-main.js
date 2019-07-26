d45076442bfc64534b415dc2f6bd774d76474ec1
smileycreations15 <git@smileycreations15.com>
2019-07-27 00:29:34 +0300

    u

--- modules/core-main.js
+++ modules/core-main.js
@@ -29,6 +29,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+var startDate = new Date()
 this.smilejs = {}
 if (this.document) loadModule("touchpolyfill.js")
 try {
@@ -186,3 +187,4 @@ smilejs.randomId = function randomId(length, chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ
         "modal": document.getElementById(id + "-modal")
       }
     }
+smilejs.meta.loadTime = new Date() - startDate
