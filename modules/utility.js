7d0a58254fef27d74eb71519f4761173d8071f69
smileycreations15 <git@smileycreations15.com>
2019-08-12 13:27:42 +0300

    update sha
    Signed-off-by: smileycreations15 <git@smileycreations15.com>

--- modules/utility.js
+++ modules/utility.js
@@ -1,9 +1,9 @@
 module.exports.ab2str = function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 module.exports.str2ab = function str2ab(str) {
   var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
+  var bufView = new Uint8Array(buf);
   for (var i=0, strLen=str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
